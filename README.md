281d0208734c181b468ca37796b92c9708d2886a
appleseed0910 <hu.xinyi@husky.neu.edu>
2018-05-10 03:29:34 -0400

    add magic.css and jquery

--- README.md
+++ README.md
@@ -5,4 +5,13 @@
 5.9 Update:
 
 . add Howler as a backup
+
 . done the [context] styling
+
+. add magic.css (for vanish animation)/ (bower json appears)
+
+. new stylesheet for controlling option positions
+
+## TODO:
+
+SOUND EFFECT!!!!!
--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+
+// howler sound effect function
 var message = new Howl({
   src: ['audio/graceful.mp3','audio/graceful.ogg']
 });
+
+// mouse motion effect test
+// for reference: 
+// https://blog.prototypr.io/stunning-hover-effects-with-css-variables-f855e7b95330
